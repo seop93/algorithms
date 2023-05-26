ee60f0a2d1915b688c230d7862e673411edf1fe6
seop93 <110223383+seop93@users.noreply.github.com>
2023-05-26 17:35:20 +0900

    first commit

---
+++ basic/array/twoPointer.py
@@ -0,0 +1,14 @@
+def twoSum(nums, target):
+    nums.sort()
+    l, r = 0, len(nums) - 1
+    while l < r:
+        if nums[l] + nums[r] == target:
+            return True
+        elif nums[l] + nums[r] <= target:
+            l += 1
+        else:
+            r -= 1
+
+    return False
+
+print(twoSum([1,23,4,6,7,23,], 7))        
