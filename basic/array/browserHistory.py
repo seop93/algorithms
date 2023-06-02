60267e7ee743d97b7095763b691e229031a3a582
seop93 <110223383+seop93@users.noreply.github.com>
2023-06-02 12:11:58 +0900

    linkedList

---
+++ basic/array/browserHistory.py
@@ -0,0 +1,14 @@
+class BrowserHistory(object):
+    def __init__(self, homepage):
+        pass
+
+    def visit(self, url):
+        pass
+
+    def back(self, stepts):
+        pass
+
+    def forward(self, steps):
+        pass
+
+    
