0729ae062618fb2ab29665dc75eda8d6093e43ed
seop93 <110223383+seop93@users.noreply.github.com>
2023-06-05 12:43:33 +0900

    LinkedList browserHistory

--- basic/array/browserHistory.py
+++ basic/array/browserHistory.py
@@ -1,14 +1,42 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None, prev=None):
+        self.val = val
+        self.next = next
+        self.prev = prev
+
+
+
 class BrowserHistory(object):
     def __init__(self, homepage):
-        self.homepage = homepage
+        self.head = self.current = ListNode(val=homepage)
 
     def visit(self, url):
-        pass
+        self.current.next = ListNode(val=url, prev=self.current)
+        self.current = self.current.next
+        return None
 
-    def back(self, stepts):
-        pass
+    def back(self, steps):
+        while steps > 0 and self.current.prev != None:
+            steps -= 1
+            self.current = self.current.prev
+        return self.current.val
 
     def forward(self, steps):
-        pass
+        while steps > 0 and self.current.next != None:
+            steps -= 1
+            self.current = self.current.next
+        return self.current.val
 
     
+
+browserHistory = BrowserHistory("leetcode.com")
+browserHistory.visit("google.com")
+browserHistory.visit("youtube.com")
+browserHistory.visit("facebook.com")
+browserHistory.back(1)
+browserHistory.back(1)
+browserHistory.forward(1)
+browserHistory.visit("linkedin.com")
+browserHistory.forward(2)
+browserHistory.back(2)
+print(browserHistory.back(7))
