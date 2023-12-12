7eb7f6a4fd437975044f161e233bd9f7fe8ed23b
seop93 <110223383+seop93@users.noreply.github.com>
2023-12-12 15:54:44 +0900

    [level 1] Title: 추억 점수 179963

--- basic/example.py
+++
@@ -1,9 +0,0 @@
-def solution(players, callings):
-    dict_sol = {key: i for i, key in enumerate(players)}
-
-    for p in callings:
-        c = dict_sol[p]
-        dict_sol[p] -= 1
-        dict_sol[players[c-1]] += 1
-        players[c-1], players[c] = players[c], players[c-1]
-    return dict_sol.keys()
