18d49f9e8f00b6412713bc7c7377f0f3e9f7f576
HEUNGSEOP <heungseopbyeon@gmail.com>
2024-10-01 21:13:52 +0900

    [level 1]

--- basic/pratice.py
+++ basic/pratice.py
@@ -1,1 +1,13 @@
-def solution(park, routes):
+import sys
+
+n=int(input())
+
+for i in range(n):
+    a,b=map(int,sys.stdin.readline().strip().split())
+    aa,bb=a,b
+
+    while bb!=0:
+        aa=aa%bb
+        aa,bb=bb,aa
+
+    print(a*b//aa)
--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,12 +1,27 @@
-import sys
+n, m = map(int, input().split())
 
-n = int(sys.stdin.readline())
-num_list = [0] * 10001
+mr = []
+cnt = []
+for i in range(n):
+    mr.append(input())
 
-for _ in range(n):
-    num_list[int(sys.stdin.readline())] += 1
+for a in range(n - 7):
+    for b in range(m - 7):
+        w_index = 0
+        b_index = 0
+        for i in range(a, a + 8):
+            for j in range(b, b + 8):
+                if (i + j) % 2 == 0:
+                    if mr[i][j] != 'W':
+                        w_index += 1
+                    else:  # W일 때
+                        b_index += 1
+                else:  # 홀수인 경우
+                    if mr[i][j] != 'W':
+                        b_index += 1
+                    else:
+                        w_index += 1
 
-for i in range(10001):
-    if num_list[i] != 0:
-        for j in range(num_list[i]):
-            print(i)
+        cnt.append(w_index)
+        cnt.append(b_index)
+print(min(cnt))
