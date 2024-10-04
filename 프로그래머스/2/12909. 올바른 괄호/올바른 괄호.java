e9810bfeeb398cb29e547296791c03df544eb2ab
seop93 <110223383+seop93@users.noreply.github.com>
2024-10-04 15:28:27 +0900

    [level 2] Title: 올바른 괄호, Time: 13.77 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,21 +1,19 @@
-
 import java.util.*;
-
 class Solution {
     boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
-        boolean answer = true;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                stack.push(s.charAt(i));
-            } else if (s.charAt(i) == ')' && stack.isEmpty()){
-                return false;
+           ArrayDeque<Character> stack = new ArrayDeque<>();
+
+        char[] a = s.toCharArray();
+        for (char c : a) {
+            if (c == '(') {
+                stack.push(c);
             } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
                 stack.pop();
             }
-
         }
-
         return stack.isEmpty();
     }
 }
